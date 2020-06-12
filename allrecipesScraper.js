d9bf0d93d04227cc658ea4c421fcd8737d82a30c
dojh111 <dojh111@gmail.com>
2020-06-12 16:11:59 +0800

    Changed to GOT package

--- allrecipesScraper.js
+++ allrecipesScraper.js
@@ -1,7 +1,6 @@
-const request = require('request');
+const got = require('got');
 const cheerio = require('cheerio');
 const fs = require('fs');
-const { format, toNamespacedPath } = require('path');
 
 const categories = ["western", "mediterranean", "indian", "chinese", "malay", "fish and chips"]
 const webLink = "https://www.allrecipes.com/search/results/?wt=";
@@ -17,79 +16,66 @@ let scrapedDataOBJ = {
     data: []
 };
 
-const webScraper = async() => {
-    //Initialise the JSON file
-    scrapedDataOBJ.data.push({ name: 'test', ratings: 'test', reviewCount: 'test', recipeURL: 'test', recipeImageURL: 'test' });
-    let jsonInitialise = JSON.stringify(scrapedDataOBJ);
-    fs.writeFile('allRecipesScraped.json', jsonInitialise, 'utf8', (err) => {
-        if (err) throw err;
-        console.log('Initialise Complete');
-    });
+const webScraper = async () => {
+    console.log('Starting webscraping');
 
     //Loop through categories
-    for (const item of categories) {
-        request(webLink + item + recipeSite, (error, response, html) => {
-            if (!error && response.statusCode == 200) {
-                const $ = cheerio.load(html);
+    for (let item of categories) {
+        try {
+            const response = await got(webLink + item + recipeSite);
+            const $ = cheerio.load(response.body);
 
-                let categoryName = item.toUpperCase();
+            let categoryName = item.toUpperCase();
 
-                console.log('---------------------------------' + categoryName + '---------------------------------');
+            console.log('---------------------------------' + categoryName + '---------------------------------');
 
-                $('.fixed-recipe-card').each(async(i, articles) => {
-                    //URL page to individual recipe
-                    const newRecipe = $(articles).find('.grid-card-image-container').find('a').attr('href');
-                    //Name of Recipe
-                    const recipeName = $(articles).find('.fixed-recipe-card__img, .ng-isolate-scope').attr('title');
-                    //URL to image of recipe
-                    const imageURL = $(articles).find('.fixed-recipe-card__img, .ng-isolate-scope').attr('data-original-src');
-                    //Ratings and Number
-                    const rating = $(articles).find('.stars, .stars-5').attr('data-ratingstars');
-                    const reviewNumber = $(articles).find('.fixed-recipe-card__reviews').contents().attr('number');
+            $('.fixed-recipe-card').each((i, articles) => {
+                //URL page to individual recipe
+                const newRecipe = $(articles).find('.grid-card-image-container').find('a').attr('href');
+                //Name of Recipe
+                const recipeName = $(articles).find('.fixed-recipe-card__img, .ng-isolate-scope').attr('title');
+                //URL to image of recipe
+                const imageURL = $(articles).find('.fixed-recipe-card__img, .ng-isolate-scope').attr('data-original-src');
+                //Ratings and Number
+                const rating = $(articles).find('.stars, .stars-5').attr('data-ratingstars');
+                const reviewNumber = $(articles).find('.fixed-recipe-card__reviews').contents().attr('number');
 
-                    //Filter out the videos
-                    if ((newRecipe !== Recipe) && !(newRecipe.includes('video'))) {
-                        Recipe = newRecipe;
-                        //Final URL to recipe page
-                        let fullLink = shortLink + Recipe;
+                //Filter out the videos
+                if ((newRecipe !== Recipe) && !(newRecipe.includes('video'))) {
+                    Recipe = newRecipe;
 
-                        //Write data to JSON file
-                        fs.readFile('allRecipesScraped.json', 'utf8', function readFileCallback(err, data) {
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                scrapedDataOBJ = JSON.parse(data);
-                                console.log(scrapedDataOBJ);
-                                scrapedDataOBJ.data.push({ id: finalTotal, name: recipeName, ratings: rating, reviewCount: reviewNumber, recipeURL: fullLink, recipeImageURL: imageURL });
-                                let json = JSON.stringify(scrapedDataOBJ);
-                                fs.writeFile('allRecipesScraped.json', json, 'utf8', (err) => {
-                                    if (err) throw err;
-                                    console.log('Complete');
-                                });
-                            }
-                        });
+                    //Final URL to recipe page
+                    let fullLink = shortLink + Recipe;
 
-                        //For Debugging Purposes
-                        // console.log('');
-                        // console.log(recipeName);
-                        // console.log(fullLink);
-                        // console.log('ImageURL: '+imageURL);
-                        // console.log('Ratings: ' + rating + '   ' + 'Review Count: ' + reviewNumber);
+                    scrapedDataOBJ.data.push({ id: finalTotal, name: recipeName, ratings: rating, reviewCount: reviewNumber, recipeURL: fullLink, recipeImageURL: imageURL });
 
-                        totalCount++;
-                        finalTotal++;
-                    }
-                });
+                    //For Debugging Purposes
+                    // console.log('');
+                    // console.log(recipeName);
+                    // console.log(fullLink);
+                    // console.log('ImageURL: ' + imageURL);
+                    // console.log('Ratings: ' + rating + '   ' + 'Review Count: ' + reviewNumber);
 
-                console.log('');
-                console.log('Total Recipes: ' + totalCount);
-                console.log('-------------------------------' + 'END OF SECTION' + '-------------------------------')
-                console.log('');
-                totalCount = 0;
-            }
-            console.log('Total: ' + finalTotal);
-        })
+                    totalCount++;
+                    finalTotal++;
+                }
+            });
+
+            console.log('');
+            console.log('Total Recipes: ' + totalCount);
+            console.log('-------------------------------' + 'END OF SECTION' + '-------------------------------')
+            console.log('');
+            totalCount = 0;
+            console.log('Global Total: ' + finalTotal);
+        } catch (error) {
+            console.log('Error: ', error);
+        }
     }
-}
+    //For Dubugging Purposes
+    //console.log(scrapedDataOBJ);
 
+    //Initialise and write to JSON file
+    let jsonInitialise = JSON.stringify(scrapedDataOBJ);
+    fs.writeFileSync('allRecipesScraped.json', jsonInitialise, 'utf8');
+}
 webScraper();
