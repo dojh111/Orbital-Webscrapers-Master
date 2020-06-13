398ab01b305c493c5bcc0f0086813dac683c205a
dojh111 <dojh111@gmail.com>
2020-06-13 12:49:56 +0800

    Bug Fixes + Web Integration

--- ARIngriedientScraper.js
+++ ARIngriedientScraper.js
@@ -2,40 +2,112 @@ const got = require('got');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-const cookingUnits = ["teaspoon", "tablespoon", "cup", "quart", "ounce", "pound", "lb", "dash", "pinch", "clove", "can", "gram", "kilogram"];
+const cookingUnits = ["teaspoon", "tablespoon", "cup", "quart", "ounce", "pound", "lb", "dash", "pinch", "clove", "gram", "kilogram"];
+const specificUnits = ["can", "large", "small"];
 
-const testPhrase = ["1 (3 pound) boneless pork butt (shoulder)", "⅓ cup hoisin sauce", "1 teaspoon kosher salt, or to taste", "⅛ teaspoon pink curing salt (optional)"];
+const testURL = ["https://www.allrecipes.com/recipe/241553/buckwheat-pancakes/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%204", "https://www.allrecipes.com/recipe/258494/chinese-barbeque-pork-char-siu/?internalSource=hub%20recipe&referringContentType=Search&clickId=cardslot%202"]
 
 const ingredientScraper = async () => {
     console.log('Starting Ingredient Scrape');
 
+    //Read JSON file into object
+
     //Cleaning up and separation of pulled ingredient
-    for (let i = 0; i < testPhrase.length; i++) {
-        let indexArray = [];
-        let item = testPhrase[i];
+    for (let url of testURL) {
+        try {
+            console.log('Loading URL...')
+            const response = await got(url);
+            const $ = cheerio.load(response.body);
 
-        //Remove commas
-        item = item.split(',', 1);
-        item = item[0].split(' ');
+            console.log('Loaded Successfully');
+            console.log('---------------------------------' + 'Recipe' + '---------------------------------');
 
-        //Find index of items with () to remove
-        for (let j = 0; j < item.length; j++) {
-            if ((item[j].indexOf('(') !== -1) || (item[j].indexOf(')') !== -1)) {
-                indexArray.push(j);
-            }
+            $('.ingredients-section li').each((i, article) => {
+                let indexArray = [];
+                let recipeUnit = 'No Unit';
+                let recipeQuantity = '';
+                let item = $(article).find('.ingredients-item-name').text();
+
+                //Trim the string
+                item = item.trim();
+
+                //Remove commas, split item and clean up
+                item = item.split(',', 1);
+                item = item[0].split(' ');
+
+                //Find index of items with () to remove
+                for (let j = 0; j < item.length; j++) {
+                    if(item[j] === '') {
+                        item.splice(j, 1);
+                    }
+                    let newObject = { toDelete: '', startIndex: '' };
+                    if (item[j].indexOf('(') !== -1) {
+                        for (let k = j; k < item.length; k++) {
+                            if (item[k].indexOf(')') !== -1) {
+                                newObject.toDelete = (k - j) + 1;
+                                newObject.startIndex = j;
+                                indexArray.push(newObject);
+                                //console.log("End Index: " +k+ "   "+"Start Index: " + j); TESTING PURPOSES
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                //Remove all items with ()
+                if (indexArray.length > 0) {
+                    for (let j = indexArray.length - 1; j >= 0; j--) {
+                        item.splice(indexArray[j].startIndex, indexArray[j].toDelete);
+                    }
+                }
+
+                //Determine units of ingredient
+                for(let j = 0; j < cookingUnits.length; j++) {
+                    if(item[1].includes(cookingUnits[j])) {
+                        recipeUnit = cookingUnits[j];
+                        item.splice(1, 1);
+                        break;
+                    }
+                }
+                if(recipeUnit === 'No Unit') {
+                    for(let j = 0; j < specificUnits.length; j++) {
+                        if(item[1] === specificUnits[j]) {
+                            recipeUnit = specificUnits[j];
+                            item.splice(1, 1);
+                            break;
+                        }
+                    }
+                }
+
+                console.log(item);
+
+                //Get ingredient quantity
+                let numberItem = Number(item[0]);
+                if(Number.isNaN(numberItem)) {
+                    recipeQuantity = item[0];
+                    item.splice(0, 1);
+                }else {
+                    recipeQuantity = numberItem;
+                    item.splice(0, 1);
+                }
+                console.log(item);
+
+                //Handle Name of Ingredient
+
+                //console.log(item);
+                console.log('Quantity: ' + recipeQuantity);
+                console.log('Unit: ' + recipeUnit);
+                console.log('');
+            })
+
+            console.log('---------------------------------' + 'End' + '---------------------------------');
+        } catch (error) {
+            console.log('Error: ', error);
         }
-
-        //Remove all items with ()
-        if (indexArray.length > 0) {
-            for (let j = indexArray.length - 1; j >= 0; j--) {
-                item.splice(indexArray[j], 1);
-            }
-        }
-        console.log(item);
     }
 }
 
 ingredientScraper();
 
 // Some Addtional notes
-// 1. All recipes uses a string to show fractions, as such cant convert to number - NaN IMPORTANT
+// 1. HTML index
